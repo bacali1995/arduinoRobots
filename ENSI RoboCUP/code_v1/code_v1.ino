80d994fe081d1cfb863dc5ac40e71f1a1e6003a9
Bac Ali Nasreddine <nasreddine.bacali95@gmail.com>
2017-09-28 08:12:57 +0100

    version 1.0.7

--- ENSI RoboCUP/code_v1/code_v1.ino
+++ ENSI RoboCUP/code_v1/code_v1.ino
@@ -4,7 +4,7 @@
 #define I2 5
 #define I3 6
 #define I4 7
-#define whiteConst 300
+#define whiteConst 95
 #define lc 300
 #define trigPin 9
 #define echoPin 10
@@ -14,26 +14,27 @@
 #define S3 33
 #define sensorOut 34
 
-
 int red = 0;
 int blue = 0;
 int green = 0;
 int frequency = 0;
 long duration;
 int distance;
-float Kp=0.001,Ki=1,Kd=1.5;
-float error=0, P=0, I=0, D=0, PID_value=0;
-float previous_error=0, previous_I=0;
-uchar sensor[16];
-uchar t;
-int initial_motor_speed=100;
+float activeSensor = 0; // Count active sensors
+float totalSensor = 0; // Total sensor readings
+float avgSensor = 4.5; // Average sensor reading
+float Kp = 60;   // Max deviation = 8-4.5 = 3.5 ||  100/3.5 = 28
+float Ki = 0.0005;
+float Kd = 6;
+float error = 0;
+float previousError = 0;
+float totalError = 0;
+int sensor[16];
+int sensorReading[8];
+float power = 0;
 
-void read_sensor_values(void);
-void caulate_pid(void);
-void motor_control(void);
-void test_distance(void);
-void test_light(void);
-void test_color(void);
+int PWM_Right, PWM_Left;
+uchar t;
 
 void setup()
 {
@@ -57,98 +58,156 @@ void setup()
 
 void loop()
 {
-    read_sensor_values();
-    calculate_pid();
+    lineFollow();
     //test_distance();
     //test_light();
     //test_color();
-    motor_control();
-    Serial.println("MOVE");
+   //delay(500);
 }
 
-void read_sensor_values()
+void readSensor()
 {
   Wire.requestFrom(9, 16);    // request 16 bytes from slave device #9
   while (Wire.available())   // slave may send less than requested
   {
-    sensor[t] = Wire.read(); // receive a byte as character
+    sensor[t] = map(Wire.read(),0,255,0,1024);
     if (t < 15)
       t++;
     else
       t = 0;
   }
-  Serial.print("sensor[0]:");
-  Serial.println(sensor[0]);
-  Serial.print("sensor[2]:");
-  Serial.println(sensor[2]);
-  Serial.println();
-  Serial.print("sensor[6]:");
-  Serial.println(sensor[6]);
-  Serial.print("sensor[8]:");
-  Serial.println(sensor[8]);
-  Serial.println();
-  Serial.print("sensor[12]:");
-  Serial.println(sensor[12]);
-  Serial.print("sensor[14]:");
-  Serial.println(sensor[14]);
-  delay(2000);
+  sensor[2]+=8;
+  sensorReading[0] = sensor[0];
+  sensorReading[1] = sensor[2];
+  sensorReading[2] = sensor[4];
+  sensorReading[3] = sensor[6];
+  sensorReading[4] = sensor[8];
+  sensorReading[5] = sensor[10];
+  sensorReading[6] = sensor[12];
+  sensorReading[7] = sensor[14];
+  for(int i=0; i<8; i++) 
+    {
+      if(sensorReading[i] <= whiteConst) {
+        activeSensor+=1; 
+        totalSensor += (i+1);
+      }
+    }
+      
+    avgSensor = totalSensor/activeSensor;
+    activeSensor = 0; totalSensor = 0;
+  /*Serial.print("[0]: ");
+  Serial.print(sensor[0]);
+ Serial.print("  [2]: ");
+  Serial.print(sensor[2]);
+  Serial.print("  [4]: ");
+  Serial.print(sensor[4]);
+  Serial.print("  [6]: ");
+  Serial.print(sensor[6]);
+  Serial.print("  [8]: ");
+  Serial.print(sensor[8]);
+  Serial.print("  [10]: ");
+  Serial.print(sensor[10]);
+  Serial.print("  [12]: ");
+  Serial.print(sensor[12]);
+  Serial.print("  [14]: ");
+  Serial.println(sensor[14]);  */
+  /*
   if((sensor[0]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]>whiteConst))
-  error=5;
+  {error=5;
+//  Serial.println("plus extreme gauche");
+}
   else if((sensor[0]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]>whiteConst)&&(sensor[14]>whiteConst))
-  error=4;
+  {error=4;
+//  Serial.println("extreme gauche");
+}
   else if((sensor[0]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]>whiteConst)&&(sensor[14]<=whiteConst))
-  error=3;
+  {error=3;
+//  Serial.println("a gauche 2");
+}
   else if((sensor[0]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]>whiteConst)&&(sensor[12]>whiteConst)&&(sensor[14]<=whiteConst))
-  error=2;
+  {error=2;
+//  Serial.println("a gauche 1");
+}
   else if((sensor[0]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]>whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]<=whiteConst))
-  error=1;
+  {error=1;
+//  Serial.println("un peu a gauche");
+}
   else if((sensor[0]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]>whiteConst)&&(sensor[8]>whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]<=whiteConst))
-  error=0;
+  {error=0;
+//  Serial.println("milieu");
+  }
   else if((sensor[0]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]>whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]<=whiteConst))
-  error=-1;
+  {error=-1;
+//  Serial.println("un peu a droite");
+  }
   else if((sensor[0]<=whiteConst)&&(sensor[2]>whiteConst)&&(sensor[6]>whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]<=whiteConst))
-  error=-2;
+ { error=-2;
+//  Serial.println("a droite 1");
+  }
   else if((sensor[0]<=whiteConst)&&(sensor[2]>whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]<=whiteConst))
-  error=-3;
+  {error=-3;
+//  Serial.println("a droite 2");
+}
   else if((sensor[0]>whiteConst)&&(sensor[2]>whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]<=whiteConst))
-  error=-4;
+  {error=-4;
+//  Serial.println("extreme droite");
+}
   else if((sensor[0]>whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[6]<=whiteConst)&&(sensor[8]<=whiteConst)&&(sensor[12]<=whiteConst)&&(sensor[14]<=whiteConst))
-  error=-5;
+ { error=-5;
+//  Serial.println("plus extreme droite");
+  }
   else if((sensor[0]<=whiteConst)&&(sensor[1]<=whiteConst)&&(sensor[2]<=whiteConst)&&(sensor[4]<=whiteConst)&&(sensor[4]<=whiteConst))
-    if(error==-5) error=-6;
-    else error=6;
+     {
+//      Serial.println("noire complet");
+     
+        if(error==-5) 
+          error=-6;
+  else error=6;}
+  */
 }
 
-void calculate_pid()
-{
-    P = error;
-    I = I + previous_I;
-    D = error-previous_error;
+void PID_program()
+{ 
+    readSensor();
     
-    PID_value = (Kp*P) + (Ki*I) + (Kd*D);
-    previous_I=I;
-    previous_error=error;
+    previousError = error; // save previous error for differential 
+    if (avgSensor-4.5>0.001) error = avgSensor - 4.5; // Count how much robot deviate from center
+    totalError += error; // Accumulate error for integral
+    
+    power = (Kp*error) + (Kd*(error-previousError)) + (Ki*totalError);
+    /*Serial.print("power = ");
+    Serial.println(power);*/
+    if( power>100 ) { power = 100.0; }
+    if( power<-100.0 ) { power = -100.0; }
+    
+    if(power<0) // Turn left
+    {
+      PWM_Right = 100;
+      PWM_Left = 100 - abs(int(power));
+    }
+    
+    else // Turn right
+    {
+      PWM_Right = 100 - int(power);
+      PWM_Left = 100;
+    }
+
+    Serial.print("PWM_Right = ");
+    Serial.print(PWM_Right);
+    Serial.print("  PWM_Left = ");
+    Serial.println(PWM_Left);
+   
 }
 
-void motor_control()
-{
-    // Calculating the effective motor speed:
-    int left_motor_speed = initial_motor_speed-PID_value;
-    int right_motor_speed = initial_motor_speed+PID_value;
-    
-    // The motor speed should not exceed the max PWM value
-    constrain(left_motor_speed,0,255);
-    constrain(right_motor_speed,0,255);
-    
-    //following lines of code are to make the bot move forward
-    /*The pin numbers and high, low values might be different
-    depending on your connections */
-    analogWrite(4,initial_motor_speed-PID_value);
-    analogWrite(5,0);
-    analogWrite(6,0);
-    analogWrite(7,initial_motor_speed+PID_value);
+void lineFollow(void) {
+   PID_program();
+   analogWrite(4,0);
+    analogWrite(5,PWM_Right);
+    analogWrite(6,PWM_Left);
+    analogWrite(7,0);
 }
 
+/********************************************************/
 void test_distance(){
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
@@ -233,10 +292,10 @@ void test_color(){
       analogWrite(7,0);
       Serial.println("STOP_COLOR");
       delay(10000);
-      analogWrite(4,initial_motor_speed-PID_value);
+      analogWrite(4,PWM_Right);
       analogWrite(5,0);
       analogWrite(6,0);
-      analogWrite(7,initial_motor_speed+PID_value);
+      analogWrite(7,PWM_Left);
       delay(500);
   } 
   
