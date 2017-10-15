f0c25072be998e4f6b2a769a91d9f6a386ac02d4
Bac Ali Nasreddine <nasreddine.bacali95@gmail.com>
2017-10-15 14:28:35 +0100

    Version 2.1

--- ENSI RoboCUP/code_v1/code_v1.ino
+++ ENSI RoboCUP/code_v1/code_v1.ino
@@ -1,18 +1,18 @@
 #include <Wire.h>
 #define uchar unsigned char
-#define I1 4
+#define I1 3
 #define I2 5
 #define I3 6
-#define I4 7
-#define whiteConst 6
+#define I4 9
+#define whiteConst 600
 #define lc 300
-#define trigPin 9
-#define echoPin 10
-#define S0 30
-#define S1 31
-#define S2 32
-#define S3 33
-#define sensorOut 34
+#define trigPin 12
+#define echoPin 13
+#define S0 2
+#define S1 4
+#define S2 10
+#define S3 11
+#define sensorOut 7
 
 int red = 0;
 int blue = 0;
@@ -26,6 +26,7 @@ int activeSensor = 0;
 
 int PWM_Right, PWM_Left;
 uchar t;
+int i = 0;
 
 void setup() {
     Wire.begin();
@@ -34,147 +35,160 @@ void setup() {
     pinMode(5, OUTPUT); //Left Motor Pin 2
     pinMode(6, OUTPUT); //Right Motor Pin 1
     pinMode(7, OUTPUT); //Right Motor Pin 2
+    pinMode(8, OUTPUT); //red light
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
     pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-    pinMode(S0, OUTPUT);
-    pinMode(S1, OUTPUT);
     pinMode(S2, OUTPUT);
     pinMode(S3, OUTPUT);
+    pinMode(22, OUTPUT);
     pinMode(sensorOut, INPUT);
-    digitalWrite(S0, HIGH);
-    digitalWrite(S1, LOW);
+
+    digitalWrite(8,LOW);
+    digitalWrite(22,LOW);
     Serial.begin(9600); //Enable Serial Communications
 }
 
-void loop() {
+void loop() {    
+    digitalWrite(8,LOW);
     lineFollow();
-    //test_distance();
-    test_light();
-    //test_color();
+    i++;
+    if (i == 500){
+      test_distance();
+      //Serial.println("ok");
+      i=0;
+    } 
+    //test_light();
+    test_color();
     //delay(500);
 }
 
 void lineFollow() {
     readSensor();
-    if (sensorReading[3] == 0 && sensorReading[4] == 1) {
-        PWM_Right = 150;
-        PWM_Left = 120;
+    if (sensorReading[1] == 0 && sensorReading[2] == 1) {
+        PWM_Right = 110;
+        PWM_Left = 90;
+        digitalWrite(22,LOW);
     }
 
-    if (sensorReading[3] == 0 && sensorReading[4] == 0) {
-        PWM_Right = 150;
-        PWM_Left = 150;
+    if (sensorReading[1] == 0 && sensorReading[2] == 0) {
+        PWM_Right = 110;
+        PWM_Left = 110;
+        //Serial.println("avant");
+        digitalWrite(22,LOW);
     }
 
-    if (sensorReading[3] == 1 && sensorReading[4] == 0) {
-        PWM_Right = 120;
-        PWM_Left = 150;
+    if (sensorReading[1] == 1 && sensorReading[2] == 0) {
+        PWM_Right = 90;
+        PWM_Left = 110;
+        digitalWrite(22,LOW);
     }
 
-    if (sensorReading[0] == 0 && sensorReading[1] == 0) {
-        PWM_Right = 200;
+    if (sensorReading[0] == 0 && sensorReading[1] == 1) {
+        PWM_Right = 110;
         PWM_Left = 0;
     }
 
-    if (sensorReading[6] == 0 && sensorReading[7] == 0) {  
+    if (sensorReading[2] == 1 && sensorReading[3] == 0) {  
         PWM_Right = 0;
-        PWM_Left = 200;
-    }
-
-    if (sensorReading[0] == 1 && sensorReading[1] == 0 && sensorReading[3] == 0 && sensorReading[4] == 1) {  
-        PWM_Right = 150;
-        PWM_Left = 40;
-    }
-
-    if (sensorReading[3] == 1 && sensorReading[4] == 0 && sensorReading[6] == 0 && sensorReading[7] == 1) {
-        PWM_Right = 40;
-        PWM_Left = 150;
+        PWM_Left = 110;
     }
 
     if (activeSensor == 0) {      
-        PWM_Right = 150;
-        PWM_Left = 150;
+        PWM_Right = 110;
+        PWM_Left = 110;
+        digitalWrite(22,LOW);
     }
-    analogWrite(4, PWM_Right);
+
+//    if ((PWM_Right == 200 && PWM_Left == 0) || (PWM_Right == 0 && PWM_Left == 200)){
+//      i++;
+//    if (i == 80){
+//      i=0;
+//    } if (i <= 40){
+//        digitalWrite(22,HIGH);
+//        Serial.println(i);
+//    } else if (i >= 40){
+//        digitalWrite(22,LOW);
+//        Serial.println(i);
+//    } 
+//    }
+    
+    analogWrite(3, PWM_Right);
     analogWrite(5, 0);
     analogWrite(6, 0);
-    analogWrite(7, PWM_Left);
+    analogWrite(9, PWM_Left);
 }
 
 void readSensor() {
-    activeSensor = 0;
-    Wire.requestFrom(9, 16); // request 16 bytes from slave device #9
-    while (Wire.available()) // slave may send less than requested
-    {
-        //sensor[t] = map(Wire.read(), 0, 255, 0, 1024);
-        sensor[t] = Wire.read();
-        if (t < 15)
-            t++;
-        else
-            t = 0;
-    }
-    for (int i = 0; i < 8; i++) {
-        sensorReading[i] = (sensor[2 * i] <= whiteConst) ? 1 : 0;
-       // Serial.print(sensorReading[i]);
+  sensorReading[0] = (analogRead(A0) <= whiteConst) ? 1 : 0;
+  sensorReading[1] = (analogRead(A1) <= whiteConst) ? 1 : 0;
+  sensorReading[2] = (analogRead(A2) <= whiteConst) ? 1 : 0;
+  sensorReading[3] = (analogRead(A3) <= whiteConst) ? 1 : 0;
+Serial.print(analogRead(A0));
+Serial.print("   ");
+Serial.print(analogRead(A1));
+Serial.print("   ");
+Serial.print(analogRead(A2));
+Serial.print("   ");
+Serial.println(analogRead(A3));
+
+    for (int i = 0; i < 4; i++) {
+//      Serial.print(sensorReading[i]);
         if (sensorReading[i] == 1) {
             activeSensor += 1;
         }
     }
-    //Serial.println();
-       /*Serial.print("[0]: ");
-       Serial.print(sensor[0]);
-       Serial.print("  [2]: ");
-       Serial.print(sensor[2]);
-       Serial.print("      [6]: ");
-       Serial.print(sensor[6]);
-       Serial.print("  [8]: ");
-       Serial.print(sensor[8]);
-       Serial.print("      [12]: ");
-       Serial.print(sensor[12]);
-       Serial.print("  [14]: ");
-       Serial.println(sensor[14]);*/
+//    Serial.print("    ");
+//       Serial.print("[0]: ");
+//       Serial.print(sensor[0]);
+//       Serial.print("  [2]: ");
+//       Serial.print(sensor[2]);
+//       Serial.print("      [6]: ");
+//       Serial.print(sensor[6]);
+//       Serial.print("  [8]: ");
+//       Serial.print(sensor[8]);
+//       Serial.print("      [12]: ");
+//       Serial.print(sensor[12]);
+//       Serial.print("  [14]: ");
+//       Serial.println(sensor[14]);
 }
 
 /********************************************************/
 
 void test_distance() {
-    // Clears the trigPin
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
 
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
 
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-
-    // Calculating the distance
-    distance = duration * 0.034 / 2;
-
-    if (distance <= 10) {
-        analogWrite(4, 0);
+ digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW); 
+  duration = pulseIn(echoPin, HIGH);
+   
+  distance = duration / 58; 
+//  Serial.print("distance = ");
+//  Serial.print(distance);
+//  Serial.println("   ");  
+    if (distance <= 20) {
+        analogWrite(3, 0);
         analogWrite(5, 0);
         analogWrite(6, 0);
-        analogWrite(7, 0);
+        analogWrite(9, 0);
+        digitalWrite(8,HIGH);
         do {
-            // Clears the trigPin
-            digitalWrite(trigPin, LOW);
-            delayMicroseconds(2);
-
             // Sets the trigPin on HIGH state for 10 micro seconds
-            digitalWrite(trigPin, HIGH);
-            delayMicroseconds(10);
-            digitalWrite(trigPin, LOW);
-
-            // Reads the echoPin, returns the sound wave travel time in microseconds
+            digitalWrite(trigPin, HIGH); 
+            delayMicroseconds(10); 
+            digitalWrite(trigPin, LOW); 
             duration = pulseIn(echoPin, HIGH);
-
-            // Calculating the distance
-            distance = duration * 0.034 / 2;
-        } while (distance <= 10);
-        delay(500);
+             
+            distance = duration / 58; 
+//            Serial.print("while =     ");
+//            Serial.print(distance);
+//            Serial.println("   ");  
+        } while (distance <= 20);
+        analogWrite(3, 0);
+        analogWrite(5, 0);
+        analogWrite(6, 0);
+        analogWrite(9, 0);
+        delay(2000);
     }
 }
 
@@ -195,7 +209,7 @@ void test_light() {
 
 /********************************************************/
 
-void test_color() {
+int color() {
     // Setting red filtered photodiodes to be read
     digitalWrite(S2, LOW);
     digitalWrite(S3, LOW);
@@ -203,8 +217,6 @@ void test_color() {
     frequency = pulseIn(sensorOut, LOW);
     red = frequency;
 
-//  Serial.print("red = ");
-//  Serial.print(red);
 
     // Setting Green filtered photodiodes to be read
     digitalWrite(S2, HIGH);
@@ -213,8 +225,6 @@ void test_color() {
     frequency = pulseIn(sensorOut, LOW);
     green = frequency;
 
-//  Serial.print("   green= ");
-//  Serial.print(green);
     // Setting Blue filtered photodiodes to be read
     digitalWrite(S2, LOW);
     digitalWrite(S3, HIGH);
@@ -222,19 +232,32 @@ void test_color() {
     frequency = pulseIn(sensorOut, LOW);
     blue = frequency;
 
-//  Serial.print("   blue = ");
-//  Serial.println(blue);
-    if (red < 120 && green > 120 && blue > 120) {
-        analogWrite(4, 0);
-        analogWrite(5, 0);
-        analogWrite(6, 0);
-        analogWrite(7, 0);
-        delay(10000);
-        analogWrite(4, PWM_Right);
-        analogWrite(5, 0);
-        analogWrite(6, 0);
-        analogWrite(7, PWM_Left);
-        
-    }
-
+    return (((blue + green)/2 - red));
 }
+
+void test_color(){
+  
+  int x1 = color();
+  int x2 = color();
+  int x = (x1+x2)/2;
+//  Serial.print("color = ");
+//  Serial.print(x);
+  if (x > 45){
+//    Serial.print("stopped = ");
+//    Serial.println(x);
+    analogWrite(3, 0);
+        analogWrite(5, 0);
+        analogWrite(6, 0);
+        analogWrite(9, 0);
+        digitalWrite(8,HIGH);
+        delay(10000);
+        analogWrite(3, PWM_Right);
+        analogWrite(5, 0);
+        analogWrite(6, 0);
+        analogWrite(9, PWM_Left);
+  }else{
+//    Serial.println();
+  }
+}
+
+
